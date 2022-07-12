a388b5e2d4deeac83c823299a3b5dce2053049f7
Daniel Guzman <dgman0628@knights.ucf.edu>
2022-07-11 22:28:14 -0400

    changed login to register

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
-import { getUser, createUser } from '../controllers/users.js';
+import { getUser, createUser } from "../controllers/users.js";
 const router = express.Router();
 
-router.get('/api/login', getUser);
-router.post('/api/login', createUser);
+router.get("/api/login", getUser);
+router.post("/api/register", createUser);
 
 export default router;
