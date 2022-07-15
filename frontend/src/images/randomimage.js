20ae98460f7fdc8eaf129da19a4629a56b5b1051
Bryan Cabrera <bryanmcabrera@gmail.com>
2022-07-15 15:28:29 -0400

    added a few states to random background images

--- frontend/src/images/randomimage.js
+++ frontend/src/images/randomimage.js
@@ -1,6 +1,14 @@
 const ri = [
-require('./stateImages/texas.png'),
-require('./stateImages/flor.png'),
-require('./stateImages/cali.png')
+require('./stateImages/TX.png'),
+require('./stateImages/FL.png'),
+require('./stateImages/CA.png'),
+require('./stateImages/NY.png'),
+require('./stateImages/NJ.png'),
+require('./stateImages/AL.png'),
+require('./stateImages/AK.png'),
+require('./stateImages/AZ.png'),
+require('./stateImages/AR.png'),
+require('./stateImages/CO.png')
+
 ];
 export default ri;
