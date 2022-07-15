108d87fbd883c03ec9a22d0ba97b52303d95c0b9
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-15 19:20:13 -0400

    changes get to post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { getUser, createUser } from '../controllers/users.js';
 const router = express.Router();
 
-router.get('/api/login', getUser);
+router.post('/api/login', getUser);
 router.post('/api/register', createUser);
 
 export default router;
