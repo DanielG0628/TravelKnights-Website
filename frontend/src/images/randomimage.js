faea50090791b1e8d60b061fda774cc74ba5e780
Bryan Cabrera <bryanmcabrera@gmail.com>
2022-07-15 16:49:36 -0400

    Added more states into Random background

--- frontend/src/images/randomimage.js
+++ frontend/src/images/randomimage.js
@@ -8,7 +8,20 @@ require('./stateImages/AL.png'),
 require('./stateImages/AK.png'),
 require('./stateImages/AZ.png'),
 require('./stateImages/AR.png'),
-require('./stateImages/CO.png')
+require('./stateImages/CO.png'),
+require('./stateImages/CT.png'),
+require('./stateImages/DE.png'),
+require('./stateImages/GA.png'),
+require('./stateImages/HI.png'),
+require('./stateImages/IL.png'),
+require('./stateImages/IN.png'),
+require('./stateImages/IA.png'),
+require('./stateImages/KS.png'),
+require('./stateImages/KY.png'),
+require('./stateImages/LA.png'),
+require('./stateImages/ME.png'),
+require('./stateImages/MD.png'),
+require('./stateImages/MA.png')
 
 ];
 export default ri;
