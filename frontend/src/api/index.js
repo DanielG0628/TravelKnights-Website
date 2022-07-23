b3e03da21ae58706bd2aac37a1e0598def3a9567
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-23 12:04:14 -0400

    actual domain

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://www.travelknights.tk:5000/api';
+const url = 'https://www.travelknightsucf.tk:5000/api';
 
 export const fetchPosts = () => axios.get(url);
 export const createUser = (user) => axios.post(`${url}/register`, user);
