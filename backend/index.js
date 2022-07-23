4bcff85083231162eee8515aae8e1f8448d5b069
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-23 00:25:00 -0400

    comment out code for test

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
-import postRoutes from "./routes/posts.js";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(filename);
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+//app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.use(express.json());
 app.use(cors());
 
 dotenv.config();
 
-app.use("/", postRoutes);
+app.use('/', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
@@ -37,25 +37,25 @@ mongoose
 //const path = require("path");
 //const { Email } = require("@mui/icons-material");
 
-app.get("/", (req, res) => res.status(200).send("it works!"));
+app.get('/', (req, res) => res.status(200).send('it works!'));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
 });
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
