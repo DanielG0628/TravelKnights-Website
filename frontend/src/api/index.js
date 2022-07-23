8a91e60b8927ca6f2a2ee5cee38a29025cc2ce74
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-23 00:26:56 -0400

    changed localhost to domain

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const url = "http://localhost:5000/api";
+const url = 'https://www.travelknights.tk:5000/api';
 
 export const fetchPosts = () => axios.get(url);
 export const createUser = (user) => axios.post(`${url}/register`, user);
