9741cbd55ae1d6d574999b36edf409cf1ae4962c
Carlos Martinez <cmartinez98@knights.ucf.edu>
2022-07-23 23:00:55 -0400

    url changes for heroku

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
+let url = '';
 
-const url = 'https://www.travelknightsucf.tk:5000/api';
+if (process.env.NODE_ENV === 'production') {
+  url = 'https://travelknights.herokuapp.com/api';
+} else {
+  url = 'http://localhost:5000/api';
+}
 
 export const fetchPosts = () => axios.get(url);
 export const createUser = (user) => axios.post(`${url}/register`, user);
